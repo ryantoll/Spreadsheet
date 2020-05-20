913475d2d09e89740f4f5e853332805114551217
Ryan Toll <toll.ryan@gmail.com>
2020-05-20 16:13:30 -0400

    Fixed linking issue for Unit Test & added tests
    -Needed to add more .obj references for windows version
    -Flipped row & column in accordance with prior change
    -Added more tests

--- UnitTest1/UnitTest1.cpp
+++ UnitTest1/UnitTest1.cpp
@@ -5,6 +5,7 @@
 #include "stdafx.h"
 #include "__Table.h"
 
+using namespace std;
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 namespace UnitTest1
@@ -14,21 +15,35 @@ namespace UnitTest1
 	public:
 		
 		// Test that functions update properly when underlying values change
-		TEST_METHOD(Function_Updates)
-		{
+		TEST_METHOD(Function_Updates) {
 			table = std::make_unique<WINDOWS_TABLE>();
-			//table = std::make_unique<CONSOLE_TABLE>();
 
 			// Insert test cells
 			CELL::cell_factory.NewCell({ 1, 1 }, "2");
-			CELL::cell_factory.NewCell({ 1, 2 }, "4");
-			CELL::cell_factory.NewCell({ 1, 3 }, "9");
-			auto cell = CELL::cell_factory.NewCell({ 1, 5 }, "=SUM( &R1C1, &R1C2, &R1C3 )");
+			CELL::cell_factory.NewCell({ 2, 1 }, "4");
+			CELL::cell_factory.NewCell({ 3, 1 }, "9");
+			auto cell = CELL::cell_factory.NewCell({ 5, 1 }, "=SUM( &R1C1, &R1C2, &R1C3 )");
 
 			CELL::cell_factory.NewCell({ 1, 1 }, "7");		// Change a cell
-			auto result = stoi( cell->DisplayOutput());
+			auto result = stoi(cell->DisplayOutput());
 
 			Assert::AreEqual(20, result);	// 7 + 4 + 9 = 20
+		}
+
+		// Purposeful error
+		TEST_METHOD(Function_Error) {
+			table = std::make_unique<WINDOWS_TABLE>();
+			auto cell = table->CreateNewCell({ 1, 1 } , "=&R5C5");
+			auto text = cell->DisplayOutput();
+			auto expectedText = "!ERROR!"s;
+			Assert::AreEqual(expectedText, text);
+		}
+
+		// Empty text returns empty cell proxy
+		TEST_METHOD(Empty_Cell) {
+			table = std::make_unique<WINDOWS_TABLE>();
+			auto cell = table->CreateNewCell({ 1, 1 } , "");
+			Assert::IsFalse(bool{ cell });
 		}
 	};
 }
