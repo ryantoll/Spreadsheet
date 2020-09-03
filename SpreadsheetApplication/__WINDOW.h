b99a768847cbc6a610a12300317cfa12ab00d7ab
Ryan Toll <toll.ryan@gmail.com>
2020-09-03 18:38:16 -0400

    WINDOW tweeks
    -Removed extraneous non-const getters from helper classes
    -Added [[nodiscard]] tag to overloaded getters to warn against potential errors
    -Extended some variable names to ensure clarity

--- SpreadsheetApplication/__WINDOW.h
+++ SpreadsheetApplication/__WINDOW.h
@@ -14,8 +14,6 @@ namespace WINDOWS_GUI {
 	public:
 		WINDOW_POSITION& X(const int x) noexcept { m_xPos = x; return *this; }
 		WINDOW_POSITION& Y(const int y) noexcept { m_yPos = y; return *this; }
-		int X() noexcept { return m_xPos; }
-		int Y() noexcept { return m_yPos; }
 		int X() const noexcept { return m_xPos; }
 		int Y() const noexcept { return m_yPos; }
 	};
@@ -25,8 +23,6 @@ namespace WINDOWS_GUI {
 	public:
 		WINDOW_DIMENSIONS& Width(const int w) noexcept { m_width = w; return *this; }
 		WINDOW_DIMENSIONS& Height(const int h) noexcept { m_height = h; return *this; }
-		int Width() noexcept { return m_width; }
-		int Height() noexcept { return m_height; }
 		int Width() const noexcept { return m_width; }
 		int Height() const noexcept { return m_height; }
 	};
@@ -67,26 +63,26 @@ namespace WINDOWS_GUI {
 		WINDOW& Message(UINT message, WPARAM wparam, LPARAM lparam) noexcept { SendMessage(m_Handle, message, wparam, lparam); return *this; }
 		const WINDOW& Message(UINT message, WPARAM wparam, LPARAM lparam) const noexcept { SendMessage(m_Handle, message, wparam, lparam); return *this; }
 
-		std::string Text() const noexcept { return Edit_Box_to_String(m_Handle); }
-		std::wstring Wtext() const noexcept { return Edit_Box_to_Wstring(m_Handle); }
+		[[nodiscard]] std::string Text() const noexcept { return Edit_Box_to_String(m_Handle); }
+		[[nodiscard]] std::wstring Wtext() const noexcept { return Edit_Box_to_Wstring(m_Handle); }
 
 		// Move, optionally resize, optionally repaint
 		// Specifying repaint == true requires that size be given as well, even if it is default-constructed
-		WINDOW& Move(const WINDOW_POSITION pos, const WINDOW_DIMENSIONS size = WINDOW_DIMENSIONS{ }, const bool repaint = true) noexcept {
-			::MoveWindow(m_Handle, pos.X(), pos.Y(), size.Width(), size.Height(), repaint);
+		WINDOW& Move(const WINDOW_POSITION position, const WINDOW_DIMENSIONS size = WINDOW_DIMENSIONS{ }, const bool repaint = true) noexcept {
+			MoveWindow(m_Handle, position.X(), position.Y(), size.Width(), size.Height(), repaint);
 			return *this;
 		}
 
 		// Move, optionally resize, optionally repaint
 		// Specifying repaint == true requires that size be given as well, even if it is default-constructed
-		WINDOW& Resize(const WINDOW_DIMENSIONS size, const WINDOW_POSITION pos = WINDOW_POSITION{ }, const bool repaint = true) noexcept {
-			return Move(pos, size, repaint);
+		WINDOW& Resize(const WINDOW_DIMENSIONS size, const WINDOW_POSITION position = WINDOW_POSITION{ }, const bool repaint = true) noexcept {
+			return Move(position, size, repaint);
 		}
 		
 		// Set the window procedure, returning the previous procedure
 		// This can be used for sub-classing a window to add additional behavior
-		WNDPROC Procedure(WNDPROC proc) noexcept { 
-			return reinterpret_cast<WNDPROC>(SetWindowLongPtr(m_Handle, GWLP_WNDPROC, reinterpret_cast<LONG_PTR>(proc)));
+		WNDPROC Procedure(WNDPROC procedure) noexcept { 
+			return reinterpret_cast<WNDPROC>(SetWindowLongPtr(m_Handle, GWLP_WNDPROC, reinterpret_cast<LONG_PTR>(procedure)));
 		}
 	};
 
