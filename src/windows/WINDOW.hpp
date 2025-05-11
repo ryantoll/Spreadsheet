660df37a5f5fd1e1f46252e4b7b83fc05ea944db
Ryan Toll <ryan.toll@protonmail.com>
2025-05-11 17:07:50 -0400

    Remove noexcept From Window Header
    - Remove noexcept specifier from window header

--- src/windows/WINDOW.hpp
+++ src/windows/WINDOW.hpp
@@ -52,19 +52,19 @@ namespace WINDOWS_GUI {
 class WINDOW_POSITION {
 	int m_xPos, m_yPos;
 public:
-	WINDOW_POSITION& X(const int x) noexcept { m_xPos = x; return *this; }
-	WINDOW_POSITION& Y(const int y) noexcept { m_yPos = y; return *this; }
-	[[nodiscard]] int X() const noexcept { return m_xPos; }
-	[[nodiscard]] int Y() const noexcept { return m_yPos; }
+	WINDOW_POSITION& X(const int x) { m_xPos = x; return *this; }
+	WINDOW_POSITION& Y(const int y) { m_yPos = y; return *this; }
+	[[nodiscard]] int X() const { return m_xPos; }
+	[[nodiscard]] int Y() const { return m_yPos; }
 };
 
 class WINDOW_DIMENSIONS {
 	int m_width, m_height;
 public:
-	WINDOW_DIMENSIONS& Width(const int w) noexcept { m_width = w; return *this; }
-	WINDOW_DIMENSIONS& Height(const int h) noexcept { m_height = h; return *this; }
-	[[nodiscard]] int Width() const noexcept { return m_width; }
-	[[nodiscard]] int Height() const noexcept { return m_height; }
+	WINDOW_DIMENSIONS& Width(const int w) { m_width = w; return *this; }
+	WINDOW_DIMENSIONS& Height(const int h) { m_height = h; return *this; }
+	[[nodiscard]] int Width() const { return m_width; }
+	[[nodiscard]] int Height() const { return m_height; }
 };
 
 // This is a wrapper class to handle classic C-style Windows API calls in an object-oriented fashion
@@ -79,78 +79,78 @@ public:
 	// Control existing window
 	explicit WINDOW(HWND hWindow) : m_Handle(hWindow) { }
 
-	HWND Handle() const noexcept { return m_Handle; }
-	operator HWND() const noexcept { return m_Handle; }			// Allow for implicit conversion to HWND
-	HWND Parent() const noexcept { return reinterpret_cast<HWND>(GetWindowLongPtr(m_Handle, GWLP_HWNDPARENT)); }
-	HINSTANCE Instance() const noexcept { return reinterpret_cast<HINSTANCE>(GetWindowLongPtr(m_Handle, GWLP_HINSTANCE)); }
-	HMENU ID() const noexcept { return Menu(); }
-	HMENU Menu() const noexcept { return reinterpret_cast<HMENU>(GetWindowLongPtr(m_Handle, GWLP_ID)); }
-	void Destroy() noexcept { DestroyWindow(m_Handle); }
-	RECT GetClientRect() const noexcept { auto rekt = RECT{ }; ::GetClientRect(m_Handle, &rekt); return rekt; }
-	WINDOW& Focus() noexcept { SetFocus(m_Handle); return *this; }
-	const WINDOW& Focus() const noexcept { SetFocus(m_Handle); return *this; }
-	WINDOW& Text(const std::string& title) noexcept { SetWindowText(m_Handle, StringToWstring(title).c_str()); return *this; }
-	const WINDOW& Text(const std::string& title) const noexcept { SetWindowText(m_Handle, StringToWstring(title).c_str()); return *this; }
-	WINDOW& Wtext(const std::wstring& title) noexcept { SetWindowText(m_Handle, title.c_str()); return *this; }
-	const WINDOW& Wtext(const std::wstring& title) const noexcept { SetWindowText(m_Handle, title.c_str()); return *this; }
-	WINDOW& Message(UINT message, WPARAM wparam, LPARAM lparam) noexcept { SendMessage(m_Handle, message, wparam, lparam); return *this; }
-	const WINDOW& Message(UINT message, WPARAM wparam, LPARAM lparam) const noexcept { SendMessage(m_Handle, message, wparam, lparam); return *this; }
-	WINDOW& Style(LONG_PTR style) noexcept { SetWindowLongPtr(m_Handle, GWL_STYLE, style); return *this; }
-	const WINDOW& Style(LONG_PTR style) const noexcept { SetWindowLongPtr(m_Handle, GWL_STYLE, style); return *this; }
+	HWND Handle() const { return m_Handle; }
+	operator HWND() const { return m_Handle; }			// Allow for implicit conversion to HWND
+	HWND Parent() const { return reinterpret_cast<HWND>(GetWindowLongPtr(m_Handle, GWLP_HWNDPARENT)); }
+	HINSTANCE Instance() const { return reinterpret_cast<HINSTANCE>(GetWindowLongPtr(m_Handle, GWLP_HINSTANCE)); }
+	HMENU ID() const { return Menu(); }
+	HMENU Menu() const { return reinterpret_cast<HMENU>(GetWindowLongPtr(m_Handle, GWLP_ID)); }
+	void Destroy() { DestroyWindow(m_Handle); }
+	RECT GetClientRect() const { auto rekt = RECT{ }; ::GetClientRect(m_Handle, &rekt); return rekt; }
+	WINDOW& Focus() { SetFocus(m_Handle); return *this; }
+	const WINDOW& Focus() const { SetFocus(m_Handle); return *this; }
+	WINDOW& Text(const std::string& title) { SetWindowText(m_Handle, StringToWstring(title).c_str()); return *this; }
+	const WINDOW& Text(const std::string& title) const { SetWindowText(m_Handle, StringToWstring(title).c_str()); return *this; }
+	WINDOW& Wtext(const std::wstring& title) { SetWindowText(m_Handle, title.c_str()); return *this; }
+	const WINDOW& Wtext(const std::wstring& title) const { SetWindowText(m_Handle, title.c_str()); return *this; }
+	WINDOW& Message(UINT message, WPARAM wparam, LPARAM lparam) { SendMessage(m_Handle, message, wparam, lparam); return *this; }
+	const WINDOW& Message(UINT message, WPARAM wparam, LPARAM lparam) const { SendMessage(m_Handle, message, wparam, lparam); return *this; }
+	WINDOW& Style(LONG_PTR style) { SetWindowLongPtr(m_Handle, GWL_STYLE, style); return *this; }
+	const WINDOW& Style(LONG_PTR style) const { SetWindowLongPtr(m_Handle, GWL_STYLE, style); return *this; }
 
-	[[nodiscard]] std::string Text() const noexcept { return EditBoxToString(m_Handle); }
-	[[nodiscard]] std::wstring Wtext() const noexcept { return EditBoxToWstring(m_Handle); }
-	[[nodiscard]] LONG_PTR Style() const noexcept { return GetWindowLongPtr(m_Handle, GWL_STYLE); }
+	[[nodiscard]] std::string Text() const { return EditBoxToString(m_Handle); }
+	[[nodiscard]] std::wstring Wtext() const { return EditBoxToWstring(m_Handle); }
+	[[nodiscard]] LONG_PTR Style() const { return GetWindowLongPtr(m_Handle, GWL_STYLE); }
 
 	// Move, optionally resize
-	WINDOW& Move(const WINDOW_POSITION position) noexcept {
+	WINDOW& Move(const WINDOW_POSITION position) {
 		SetWindowPos(m_Handle, nullptr, position.X(), position.Y(), 0, 0, SWP_NOZORDER | SWP_NOSIZE);
 		return *this;
 	}
 
 	// Move, optionally resize
-	const WINDOW& Move(const WINDOW_POSITION position) const noexcept {
+	const WINDOW& Move(const WINDOW_POSITION position) const {
 		SetWindowPos(m_Handle, nullptr, position.X(), position.Y(), 0, 0, SWP_NOZORDER | SWP_NOSIZE);
 		return *this;
 	}
 
 	// Move, optionally resize
-	WINDOW& Move(const WINDOW_POSITION position, const WINDOW_DIMENSIONS size) noexcept {
+	WINDOW& Move(const WINDOW_POSITION position, const WINDOW_DIMENSIONS size) {
 		SetWindowPos(m_Handle, nullptr, position.X(), position.Y(), size.Width(), size.Height(), SWP_NOZORDER);
 		return *this;
 	}
 
 	// Move, optionally resize
-	const WINDOW& Move(const WINDOW_POSITION position, const WINDOW_DIMENSIONS size) const noexcept {
+	const WINDOW& Move(const WINDOW_POSITION position, const WINDOW_DIMENSIONS size) const {
 		SetWindowPos(m_Handle, nullptr, position.X(), position.Y(), size.Width(), size.Height(), SWP_NOZORDER);
 		return *this;
 	}
 
 	// Resize, optionally move
-	WINDOW& Resize(const WINDOW_DIMENSIONS size) noexcept {
+	WINDOW& Resize(const WINDOW_DIMENSIONS size) {
 		SetWindowPos(m_Handle, nullptr, 0, 0, size.Width(), size.Height(), SWP_NOZORDER | SWP_NOMOVE);
 		return *this;
 	}
 
 	// Resize, optionally move
-	const WINDOW& Resize(const WINDOW_DIMENSIONS size) const noexcept {
+	const WINDOW& Resize(const WINDOW_DIMENSIONS size) const {
 		SetWindowPos(m_Handle, nullptr, 0, 0, size.Width(), size.Height(), SWP_NOZORDER | SWP_NOMOVE);
 		return *this;
 	}
 
 	// Resize, optionally move
-	WINDOW& Resize(const WINDOW_DIMENSIONS size, const WINDOW_POSITION position) noexcept {
+	WINDOW& Resize(const WINDOW_DIMENSIONS size, const WINDOW_POSITION position) {
 		return Move(position, size);
 	}
 
 	// Resize, optionally move
-	const WINDOW& Resize(const WINDOW_DIMENSIONS size, const WINDOW_POSITION position) const noexcept {
+	const WINDOW& Resize(const WINDOW_DIMENSIONS size, const WINDOW_POSITION position) const {
 		return Move(position, size);
 	}
 
 	// Set the window procedure, returning the previous procedure
 	// This can be used for sub-classing a window to add additional behavior
-	WNDPROC Procedure(WNDPROC procedure) noexcept {
+	WNDPROC Procedure(WNDPROC procedure) {
 		return reinterpret_cast<WNDPROC>(SetWindowLongPtr(m_Handle, GWLP_WNDPROC, reinterpret_cast<LONG_PTR>(procedure)));
 	}
 };
