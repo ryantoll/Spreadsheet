58e774ccf06fe32a6ea9554a8f7c39068320c682
Ryan Toll <ryan.toll@protonmail.com>
2025-05-11 12:44:18 -0400

    Start Real Unit Tests
    - Create a basic table mock
    - Create two real unit tests

--- tests/test.cpp
+++ tests/test.cpp
@@ -3,32 +3,65 @@
 #include "Cell.hpp"
 #include "Table.hpp"
 
-/// @todo create test table to store cells
-/// may need to split out dependency in some way
+class TEST_TABLE : public TABLE_BASE {
+public:
+	void InitializeTable() override { };
+	void PrintCellList() const { };
+	void Redraw() const override { };
+	void Undo() const override { };
+	void Redo() const override { };
+	CELL::CELL_PROXY CreateNewCell() const { return CELL::CELL_PROXY{ }; };
+	CELL::CELL_PROXY CreateNewCell(const CELL::CELL_POSITION, const std::string&) const override { return CELL::CELL_PROXY{ nullptr }; };
+	void ClearCell(const CELL::CELL_POSITION) const { };
+	CELL::CELL_POSITION RequestCellPos() const { return CELL::CELL_POSITION{ }; };
+protected:
+	mutable CELL::CELL_DATA cellData;
+	mutable std::vector<std::pair<CELL::CELL_PROXY, CELL::CELL_PROXY>> undoStack{ };
+	mutable std::vector<std::pair<CELL::CELL_PROXY, CELL::CELL_PROXY>> redoStack{ };
+
+	// Unused functions
+	void Resize() override { }
+	void AddRow() override { }
+	void AddColumn() override { }
+	void RemoveRow() override { }
+	void RemoveColumn() override { }
+	unsigned int GetNumColumns() const override { return 0; }
+	unsigned int GetNumRows() const override { return 0; }
+	void FocusCell(const CELL::CELL_POSITION) const override { }
+	void UnfocusCell(const CELL::CELL_POSITION) const override { }
+	void FocusEntryBox() const override { }
+	void UnfocusEntryBox(const CELL::CELL_POSITION) const override { }
+	void FocusUp1(const CELL::CELL_POSITION) const override { }
+	void FocusDown1(const CELL::CELL_POSITION) const override { }
+	void FocusRight1(const CELL::CELL_POSITION) const override { }
+	void FocusLeft1(const CELL::CELL_POSITION) const override { }
+	void LockTargetCell(const CELL::CELL_POSITION) const override { }
+	void ReleaseTargetCell() const override { }
+	void UpdateCell(const CELL::CELL_POSITION) const override { };
+	CELL::CELL_POSITION TargetCellGet() const override { return CELL::CELL_POSITION{ }; }
+};
 
 TEST_CASE("Invalid Position Returns No Cell") {
-	CHECK(true);
+	table = std::make_unique<TEST_TABLE>();
 
-	//auto invalidPosition = CELL::CELL_POSITION{ 1, 1 };
-	//constexpr auto rawText = std::string_view{ "TEST" };
-	//auto cellData = CELL::CELL_DATA{};
-	//auto cell = CELL::NewCell(&cellData, CELL::CELL_POSITION{ 1, 1 }, std::string{ rawText });
-	//REQUIRE_FALSE(bool{ cell });
-
-	//auto cell = table->CreateNewCell(CELL::CELL_POSITION{ 1, 1 }, std::string{ rawText });
-	//REQUIRE_FALSE(bool{ cell });
-	//REQUIRE(bool{ cell });
+	auto invalidPosition = CELL::CELL_POSITION{ 0, 0 };
+	constexpr auto rawText = std::string_view{ "TEST" };
+	auto cellData = CELL::CELL_DATA{ };
+	auto cell = CELL::NewCell(&cellData, invalidPosition, std::string{ rawText });
+	CHECK_FALSE(bool{ cell });
 }
 
 
-TEST_CASE("Text Cell Displays Text") {
-	CHECK(true);
-//	auto position = CELL::CELL_POSITION{ 1, 1 };
-//	constexpr auto rawText = std::string_view{ "TEST" };
-//
-//	auto cell = table->CreateNewCell(CELL::CELL_POSITION{ 1, 1 }, std::string{ rawText });
-//	REQUIRE(cell->GetPosition() == position);
-//	REQUIRE(cell->GetRawContent() == rawText);
-//	REQUIRE(cell->GetOutput() == rawText);
+TEST_CASE("Text Cell Displays Matching Text") {
+	table = std::make_unique<TEST_TABLE>();
+	auto cellData = CELL::CELL_DATA{ };
+	auto position = CELL::CELL_POSITION{ 1, 1 };
+	auto rawText = "TEST";
+
+	auto cell = CELL::NewCell(&cellData, position, std::string{ rawText });
+	REQUIRE(bool{ cell });
+	CHECK(cell->GetPosition() == position);
+	CHECK(cell->GetRawContent() == rawText);
+	CHECK(cell->GetOutput() == rawText);
 }
 
